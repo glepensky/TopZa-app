b01839f384db6ad4683d387eec43a1e29a4d5fd8
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-10-25 19:55:07 -0500

    added scripts to welcome and about pages

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,12 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>I love pizza. 
+            Pizza is my Roman Empire. 
+            In the words of comedian Mike Birbiglia, I love pizza so much I get excited when I see the word “plaza”. 
+            I grew up in New Haven, Connecticut, which is famously known for its pizza, or ‘apizza’. Apizza is a style of pizza with a thin-crust, coal-charred crust and delicious “mootz” 🤌 forged in the fires of the Neapolitan technique. We have three pizzerias that we fondly call the Holy Trinity - Frank Pepe’s, Sally’s Apizza, and Modern Apizza. We are also quite creative with white clam pizza, mashed potato pizza, the Brussel Hustle... the list could go on. 
+            I created TopZa as both a celebration of pizza - a place to take meticulous notes and highly debatable ratings - as well as a way to test and push my programming skills. I hope you enjoy it! 
+            Happy pizza eating! 🍕</p>
       </div>
     </div>
   );
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -20,36 +20,12 @@ function LandingPage() {
       <div className="grid">
         <div className="grid-col grid-col_8">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
+          Introducing TopZa: Your ultimate companion for pizza enthusiasts! 
+            With TopZa, you can easily discover, rate, and keep track of all the incredible pizza places you've tried. 
+            From the perfect New York slice to mouthwatering deep-dish delights, our app lets you share your pizza adventures, find new favorites, and never forget a memorable slice. 
+            oin our community of pizza lovers and embark on a delicious journey with TopZa today!
           </p>
 
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
         </div>
         <div className="grid-col grid-col_4">
           <RegisterForm />
