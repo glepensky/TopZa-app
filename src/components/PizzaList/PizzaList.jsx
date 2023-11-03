21db5f354758b545a53477a60bd00c7f73c35dc3
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-11-02 20:24:46 -0500

    add to pizza list button

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function PizzaList() {
-    return (
-        <div>
-            <h1>Here's your pizza list!</h1>
-            {/* Any other content you want on this page */}
+  const [pizzaList, setPizzaList] = useState([]);
+
+  useEffect(() => {
+    // Load the pizza list from local storage on component mount
+    const storedPizzaList = JSON.parse(localStorage.getItem('pizzaList') || '[]');
+    setPizzaList(storedPizzaList);
+  }, []);
+
+  if (pizzaList.length === 0) {
+    return <div>No pizzas in your list yet!</div>;
+  }
+
+  return (
+    <div>
+      <h2>Your Pizza List</h2>
+      {pizzaList.map((pizza, index) => (
+        <div key={index}>
+          <h3>{pizza.name}</h3>
+          <p>{pizza.location}</p>
+          {/* Additional pizza details */}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default PizzaList;
--- src/components/SelectedResult/SelectedResult.jsx
+++ src/components/SelectedResult/SelectedResult.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+// Import axios to make HTTP requests
+import axios from 'axios';
 
 function SelectedResult() {
   const [result, setResult] = useState(null);
@@ -20,6 +22,19 @@ function SelectedResult() {
     }
   }, []);
 
+  // Event handler for adding to pizza list
+  const handleAddToPizzaList = () => {
+    // Implement logic to add the restaurant to the pizza list
+    // This might involve updating the state or local storage, or making an API call
+    // For example, if using local storage:
+    const pizzaList = JSON.parse(localStorage.getItem("pizzaList") || "[]");
+    const newPizzaList = [...pizzaList, result];
+    localStorage.setItem("pizzaList", JSON.stringify(newPizzaList));
+
+    // Optionally, confirm to the user that it was added
+    alert("Added to your Pizza List!");
+  };
+
   if (error) {
     return <div>Error: {error}</div>;
   }
@@ -38,6 +53,8 @@ function SelectedResult() {
       <div>
         <h3>🍕 {result.name}</h3>
         {location}
+        {/* Add restaurant button */}
+        <button onClick={handleAddToPizzaList}>Add to Pizza List</button>
       </div>
     </div>
   );
