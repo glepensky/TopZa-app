13e371876d316845f3011b7112966695a710a638
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-11-06 18:55:16 -0600

    added bono mascot photo

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import bonopizza from './bonopizza.png';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -22,7 +22,7 @@ function AboutPage() {
             I grew up in New Haven, Connecticut, which is famously known for its pizza, or ‘apizza’. Apizza is a style of pizza with a thin-crust, coal-charred crust and delicious “mootz” 🤌 forged in the fires of the Neapolitan technique. We have three pizzerias that we fondly call the Holy Trinity - Frank Pepe’s, Sally’s Apizza, and Modern Apizza. We are also quite creative with white clam pizza, mashed potato pizza, the Brussel Hustle... the list could go on. 
             I created TopZa as both a celebration of pizza - a place to take meticulous notes and highly debatable ratings - as well as a way to test and push my programming skills. I hope you enjoy it! 
             Happy pizza eating! 🍕</p>
-            
+            <img src={ bonopizza } alt="bono logo"/>
       </div>
     </div>
   );
